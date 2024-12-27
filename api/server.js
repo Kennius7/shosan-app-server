47fc6483061d2921a1efd275126b411c2b1f0a26
Kennius7 <ogbogukenny@yahoo.com>
2024-12-27 22:28:13 +0100

    server config creation - 06

--- api/server.js
+++ api/server.js
@@ -4,9 +4,40 @@ const { useAuthState } = require("react-firebase-hooks/auth");
 
 
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../shosan-acodemia-app-firebase-adminsdk-y3xc9-5128ebd3c2.json");
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: 'https://shosan-acodemia-app.firebaseio.com', // Replace with your database URL
+    });
+}
+// admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+
+async function verifyToken(idToken) {
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        const userRecord = await admin.auth().getUser(decodedToken.uid);
+
+        return {
+            userId: userRecord.uid,
+            email: userRecord.email,
+            displayName: userRecord.displayName,
+            photoURL: userRecord.photoURL,
+        };
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        throw new Error('Invalid token');
+    }
+}
+
+
 export default async function handler(req, res) {
     console.log("Checking...");
     const [ currentlyLoggedInUser ] = useAuthState(auth);
+    const idToken = req.headers.authorization?.split('Bearer ')[1];
 
     if (req.method === "OPTIONS") {
         res.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,6 +45,10 @@ export default async function handler(req, res) {
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         res.status(200).end();
         return;
+    }
+
+    if (!idToken) {
+        return res.status(401).json({ success: false, message: 'Unauthorized: No token provided' });
     }
 
 
@@ -37,7 +72,9 @@ export default async function handler(req, res) {
     // Fetching User Data Block
     if (req.method === "GET") {
         try {
-            const userEmail = currentlyLoggedInUser?.email;
+            // const userEmail = currentlyLoggedInUser?.email;
+            const userData = await verifyToken(idToken);
+            const userEmail = userData.email;
             console.log("Current User Email: ", userEmail);
             const q = query(collection(db, "User_Data"), where("email", "==", userEmail));
             const querySnapshot = await getDocs(q);
@@ -58,7 +95,7 @@ export default async function handler(req, res) {
             res.status(200).json({ data: fetchedData, message: "Data was fetched successfully" });
             console.log(fetchedData);
         } catch (error) {
-            res.json({ error: `Error: ${error.message}` });
+            res.json({ error: `Couldn't fetch Data. Error: ${error.message}` });
         }
     } else res.status(405).json({ error: "Method not allowed" });
 }
