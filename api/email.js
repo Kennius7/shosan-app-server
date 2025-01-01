8910185f4030100e36ec2571fec4cfd2be02274b
Kennius7 <ogbogukenny@yahoo.com>
2025-01-01 19:47:37 +0100

    server config creation - 22

--- api/email.js
+++ api/email.js
@@ -3,17 +3,63 @@ require('dotenv').config();
 
 
 
-// Configure the email transport
-const transporter = nodemailer.createTransport({
-    service: 'gmail', // e.g., 'gmail'
-    auth: {
-        user: 'shosanacodemia@gmail.com',
-        pass: 'mgkr dhey vfry zdrc',
-    },
-    tls: {
-        rejectUnauthorized: false, // Bypass SSL certificate validation
-    },
-});
+const userEmail = process.env.NODEMAILER_USER_EMAIL;
+const userPassword = process.env.NODEMAILER_USER_PASSWORD;
+
+
+const emailTransporter = (data, res) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', // e.g., 'gmail'
+        auth: {
+            user: userEmail,
+            pass: userPassword,
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
+    });
+
+    const htmlEmail = `
+        <html>
+            <body>
+                <p>${data.name} just sent an email.</p>
+                <p>Phone/WhatSapp number: ${data.number}</p>
+                <p>Email address: ${data.email}</p>
+                <p>Email Subject Header: ${data.subject}</p>
+                <p>Email: ${data.message}</p>
+            </body>
+        </html>
+    `;
+
+    const mailOptions = {
+        from: userEmail,
+        to: userEmail,
+        subject: data.subject,
+        html: htmlEmail,
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            errorData = error;
+            return res.status(500).json({ 
+                success: false, 
+                timeoutMessage: "Error: Request Timed Out. Check your network and try again.",
+                errorMessage: "Error: Registration Failed.",
+            });
+        } else {
+            console.log('Email sent: ' + info.response);
+            infoData = info;
+            return res.status(200).json({ 
+            success: true, 
+            emailMessage: `Email sent successfully: ${info.response}`, 
+            formMessage: 'Registration successful.'
+            });
+        }
+    });
+}
+
+
 
 export default async function handler(req, res) {
     console.log("Checking...");
@@ -30,48 +76,19 @@ export default async function handler(req, res) {
 
       // Sending Email Block
     if (req.method === "POST") {
-        const formData = req.body;
-        console.log(formData);
-        const subject = "Shosan Code Hub Emails";
-        const htmlEmail = `
-            <html>
-                <body>
-                <p>${formData.name} just registered for the upcoming ${formData.courses} Online Class.</p>
-                <p>His/Her Phone/WhatSapp number is: ${formData.number}</p>
-                <p>His/Her Email address is: ${formData.email}</p>
-                </body>
-            </html>
-        `;
-    
-        const mailOptions = {
-            from: 'shosanacodemia@gmail.com',
-            to: 'shosanacodemia@gmail.com',
-            subject: subject,
-            html: htmlEmail,
+        try {
+            const formData = req.body;
+            emailTransporter(formData, res);
+            return res.status(200).json({ success: true, message: 'Email sent successfully' });
+        } catch (error) {
+            console.error("Error: ", error);
+            return res.status(500).json({ success: false, message: 'Internal Server Error' });
         }
-    
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error(error);
-                res.status(500).json({ 
-                    success: false, 
-                    timeoutMessage: "Error: Request Timed Out. Check your network and try again.",
-                    errorMessage: "Error: Registration Failed.",
-                });
-            } else {
-                console.log('Email sent: ' + info.response);
-                res.status(200).json({ 
-                success: true, 
-                emailMessage: `Email sent successfully: ${info.response}`, 
-                formMessage: 'Registration successful.'
-                });
-            }
-        });
     }
 
-    if (req.method !== 'POST' || req.method !== 'GET' || req.method !== 'OPTIONS') {
+    if (!['POST', 'GET', 'OPTIONS'].includes(req.method)) {
+        console.log("Method not allowed");
         return res.status(405).json({ success: false, message: 'Method not allowed' });
     }
 }
-
 
